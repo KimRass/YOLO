03587bebe91dc3af8645d6fa9ee010bfcee6c156
KimRass <rmx1000@yonsei.ac.kr>
2023-08-02 00:06:15 +0900

    Data aug

--- voc2012.py
+++ voc2012.py
@@ -25,10 +25,30 @@ from utils import (
 )
 
 VOC_CLASSES = [
-    "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor"
 ]
-
 IMG_SIZE = 448
+RATIO = 0.2
 
 # "To avoid overfitting we use dropout and extensive data augmentation. A dropout layer with rate = .5 after the first connected layer prevents co-adaptation between layers [18]. For data augmentation we introduce random scaling and translations of up to 20% of the original image size. We also randomly adjust the exposure and saturation of the image by up to a factor of 1:5 in the HSV color space. 2.3. Inference Just like in training, predicting"
 
@@ -45,7 +65,7 @@ def parse_xml_file(xml_path):
             int(bbox.find("bndbox").find("ymax").text),
             VOC_CLASSES.index(xroot.find("object").find("name").text)
         ) for bbox in xroot.findall("object")
-    ], columns=("x1", "y1", "x2", "y2", "label"))
+    ], columns=("x1", "y1", "x2", "y2", "image"))
 
     img_path = Path(xml_path).parent.parent/"JPEGImages"/xroot.find("filename").text
     image = Image.open(img_path).convert("RGB")
@@ -139,23 +159,11 @@ class VOC2012Dataset(Dataset):
         self.root = root
         self.transform = transform
 
-    # def resize(image, bboxes):
-    #     w, h = image.size
-    #     image = TF.center_crop(image, output_size=max(h, w))
-    #     image = TF.resize(image, size=IMG_SIZE, antialias=True)
-        
-    #     scale = IMG_SIZE / max(w, h)
-    #     pad = round((IMG_SIZE - w * scale) // 2), round((IMG_SIZE - h * scale) // 2)
-    #     bboxes["x1"] = bboxes["x1"].apply(lambda x: round(x * scale + pad[0]))
-    #     bboxes["y1"] = bboxes["y1"].apply(lambda x: round(x * scale + pad[1]))
-    #     bboxes["x2"] = bboxes["x2"].apply(lambda x: round(x * scale + pad[0]))
-    #     bboxes["y2"] = bboxes["y2"].apply(lambda x: round(x * scale + pad[1]))
-    #     return image, bboxes
-
-    def _random_scale(image, bboxes):
-        h, w = image.size
-        scale = random.uniform(1 - 0.2, 1 + 0.2)
-        image = TF.resize(image, size=(round(w * scale), round(h * scale)), antialias=True)
+    # "We introduce random scaling and translations of up to 20% of the original image size."
+    def _randomly_scale(image, bboxes, ratio=RATIO):
+        w, h = image.size
+        scale = random.uniform(1 - ratio, 1 + ratio)
+        image = TF.resize(image, size=(round(h * scale), round(w * scale)), antialias=True)
 
         bboxes["x1"] = bboxes["x1"].apply(lambda x: round(x * scale))
         bboxes["y1"] = bboxes["y1"].apply(lambda x: round(x * scale))
@@ -163,40 +171,18 @@ class VOC2012Dataset(Dataset):
         bboxes["y2"] = bboxes["y2"].apply(lambda x: round(x * scale))
         return image, bboxes
 
-    def shift_image(image, ori_w, ori_h):
-        dx = round(ori_w * random.uniform(-0.2, 0.2))
-        dy = round(ori_h * random.uniform(-0.2, 0.2))
-
-        img = np.array(image)
-        img = np.roll(img, dy, axis=0)
-        img = np.roll(img, dx, axis=1)
-        if dy > 0:
-            img[: dy, :] = 0
-        elif dy < 0:
-            img[dy:, :] = 0
-        if dx > 0:
-            img[:, : dx] = 0
-        elif dx < 0:
-            img[:, dx:] = 0
-        image = Image.fromarray(img)
-        return image
-
-    def _resize(image):
+    def _randomly_shift_and_resize(image, ori_w, ori_h, ratio=RATIO):
+        # We also randomly adjust the exposure and saturation of the image by up to a factor of 1:5 in the HSV color space."
+        dx = round(ori_w * random.uniform(-ratio, ratio))
+        dy = round(ori_h * random.uniform(-ratio, ratio))
         w, h = image.size
-        img = np.array(image)
-        if w > IMG_SIZE:
-            img = img[:, w // 2 - IMG_SIZE // 2: w // 2 + IMG_SIZE // 2, :]
-        if h > IMG_SIZE:
-            img = img[h // 2 - IMG_SIZE // 2: h // 2 + IMG_SIZE // 2, :, :]
-
-        h, w, _ = img.shape
-        canvas = np.zeros((IMG_SIZE, IMG_SIZE, 3), dtype="uint8")
-        canvas[
-            IMG_SIZE // 2 - h // 2: IMG_SIZE // 2 - h // 2 + h,
-            IMG_SIZE // 2 - w // 2: IMG_SIZE // 2 - w // 2 + w,
-            :
-        ] = img
-        image = Image.fromarray(canvas)
+        padding=(
+            (IMG_SIZE - w) // 2 + dx,
+            (IMG_SIZE - h) // 2 + dy,
+            IMG_SIZE - w - (IMG_SIZE - w) // 2 - dx,
+            IMG_SIZE - h - (IMG_SIZE - h) // 2 - dy,
+        )
+        image = TF.pad(image, padding=padding, padding_mode="edge")
         return image
 
     def __len__(self):
@@ -205,96 +191,12 @@ class VOC2012Dataset(Dataset):
     def __getitem__(self, idx):
         xml_path = list(Path(self.root).glob("*.xml"))[idx]
         xml_path = "/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/Annotations/2007_000032.xml"
-        image, bboxes = parse_xml_file(xml_path)
-        ori_w, ori_h = image.size
-        image, bboxes = _random_scale(image=image, bboxes=bboxes)
-        image = shift_image(image, ori_w=ori_w, ori_h=ori_h)
-        image = _resize(image)
-        image.show()
-
-        
-        
-
-        print(w, h)
-        print(canvas[
-            IMG_SIZE // 2 - h // 2: IMG_SIZE // 2 - h // 2 + h,
-            IMG_SIZE // 2 - w // 2: IMG_SIZE // 2 - w // 2 + w,
-            :
-        ].shape)
-        canvas[
-            IMG_SIZE // 2 - h // 2: IMG_SIZE // 2 - h // 2 + h,
-            IMG_SIZE // 2 - w // 2: IMG_SIZE // 2 - w // 2 + w,
-            :
-        ] = np.array(image)
-        return Image.fromarray(canvas)
-        w, h
-        (IMG_SIZE // 2 - h, IMG_SIZE // 2 + h)
-
-
-        show_image(img)
-        show_image(img)
-    
-        center_x, center_y
-        canvas
-        
-        center_x, center_y = IMG_SIZE // 2, IMG_SIZE // 2
-        x1 = center_x - min(w, IMG_SIZE) // 2
-        y1 = center_y - min(h, IMG_SIZE) // 2
-        x2 = center_x + min(w, IMG_SIZE) // 2
-        y2 = center_y + min(h, IMG_SIZE) // 2
-        
-        canvas[y1: y2, x1: x2, :] = img[y1: y2, x1: x2, :]
-        w, h
-        
-        
-        
-        
-        
-        (center_x, center_y), (w, h)
-        
-        x1 = max(0, center_x - IMG_SIZE // 2)
-        y1 = max(0, center_y - IMG_SIZE // 2)
-        x2 = min(w, center_x + IMG_SIZE // 2)
-        y2 = min(h, center_y + IMG_SIZE // 2)
-        x1, y1, x2, y2
-        
-        x1 = max(0, center_x - w // 2)
-        y1 = max(0, center_y - h // 2)
-        x2 = min(w, center_x + w // 2)
-        y2 = min(h, center_y + h // 2)
-        # x1, y1, x2, y2 = center_x - w // 2, center_y - h // 2, center_x + w // 2, center_y + h // 2
-        canvas[y1: y2, x1: x2, :] = np.array(image)
-        np.array(image).shape
-        h
-        
-        
-        
-        
-    
-        drawn = draw_bboxes(image=image, bboxes=bboxes)
-        drawn.show()
-        
-        
-        
-        ratio = h / w
-        image, bboxes = resize(image=image, bboxes=bboxes)
-
-
-        IMG_SIZE, IMG_SIZE / ratio
-
-
-        
-        xroot.find("filename").text
-        Image.open(
-        img_path = Path(xml_path).parent.parent/"JPEGImages"/xroot.find("filename").text)
-        temp = _normalize_bboxes_coordinates(bboxes=bboxes, img=img)
-        bboxes
-        temp
-        gt = generate_ground_truth(img=img, bboxes=bboxes)
-        image = _to_pil(img)
-        if self.transform is not None:
-            image = self.transform(image)
-        return image, gt
+        for _ in range(10):
+            image, bboxes = parse_xml_file(xml_path)
+            ori_w, ori_h = image.size
+            image, bboxes = _randomly_scale(image=image, bboxes=bboxes)
+            image = _randomly_shift_and_resize(image, ori_w=ori_w, ori_h=ori_h)
+            image.show()
 
 
 if __name__ == "__main__":
@@ -311,5 +213,3 @@ if __name__ == "__main__":
         darknet = Darknet()
         yolo = YOLO(darknet=darknet, n_classes=20)
         pred = yolo(image)
-
-# "we introduce random scaling and translations of up to 20% of the original image size. We also randomly adjust the exposure and saturation of the image by up to a factor of 1:5 in the HSV color space."
