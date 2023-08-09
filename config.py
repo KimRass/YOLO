4eaf9b698434d2249652d758127dea718651b4f7
KimRass <rmx1000@yonsei.ac.kr>
2023-08-09 18:56:10 +0900

    Data augmentation and generate gt

---
+++ config.py
@@ -0,0 +1,27 @@
+### Data
+VOC_CLASSES = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor"
+]
+IMG_SIZE = 448
+TRANSFORM_RATIO = 0.2
+ANNOT_DIR = "/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/Annotations"
--- voc2012.py
+++ voc2012.py
@@ -12,9 +12,9 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
-import extcolors
 import random
 
+import config
 from utils import (
     _to_array,
     _to_pil,
@@ -48,7 +48,9 @@ VOC_CLASSES = [
     "tvmonitor"
 ]
 IMG_SIZE = 448
-RATIO = 0.2
+TRANSFORM_RATIO = 0.2
+N_CELLS = 7
+CELL_SIZE = IMG_SIZE // N_CELLS
 
 # "To avoid overfitting we use dropout and extensive data augmentation. A dropout layer with rate = .5 after the first connected layer prevents co-adaptation between layers [18]. For data augmentation we introduce random scaling and translations of up to 20% of the original image size. We also randomly adjust the exposure and saturation of the image by up to a factor of 1:5 in the HSV color space. 2.3. Inference Just like in training, predicting"
 
@@ -65,104 +67,47 @@ def parse_xml_file(xml_path):
             int(bbox.find("bndbox").find("ymax").text),
             VOC_CLASSES.index(xroot.find("object").find("name").text)
         ) for bbox in xroot.findall("object")
-    ], columns=("x1", "y1", "x2", "y2", "image"))
+    ], columns=("x1", "y1", "x2", "y2", "cls"))
 
     img_path = Path(xml_path).parent.parent/"JPEGImages"/xroot.find("filename").text
     image = Image.open(img_path).convert("RGB")
     return image, bboxes
 
 
-def _normalize_bboxes_coordinates(bboxes, img, img_size=IMG_SIZE):
-    copied = bboxes.copy()
-
-    img_w, img_h = _get_width_and_height(img)
-    if img_w >= img_h:
-        resize_ratio = img_size / img_w
-        new_h = round(img_h * resize_ratio)
-        pad = (img_size - new_h) // 2
-
-        copied["x1"] = copied["x1"].apply(lambda x: round(x * resize_ratio))
-        copied["x2"] = copied["x2"].apply(lambda x: round(x * resize_ratio))
-        copied["y1"] = copied["y1"].apply(lambda x: round(x * resize_ratio + pad))
-        copied["y2"] = copied["y2"].apply(lambda x: round(x * resize_ratio + pad))
-    else:
-        resize_ratio = img_size / img_h
-        new_w = round(img_w * resize_ratio)
-        pad = (img_size - new_w) // 2
-
-        copied["x1"] = copied["x1"].apply(lambda x: round(x * resize_ratio + pad))
-        copied["x2"] = copied["x2"].apply(lambda x: round(x * resize_ratio + pad))
-        copied["y1"] = copied["y1"].apply(lambda x: round(x * resize_ratio))
-        copied["y2"] = copied["y2"].apply(lambda x: round(x * resize_ratio))
-    return copied
-
-
-def generate_ground_truth(img, bboxes, img_size=IMG_SIZE, n_cells=7):
-    copied = bboxes.copy()
-    ### Normalize
-    copied = _normalize_bboxes_coordinates(bboxes=copied, img=img)
-
-    cell_size = img_size // n_cells
-    copied["x"] = copied.apply(
-        lambda x: (((x["x1"] + x["x2"]) / 2) % cell_size) / cell_size,
-        axis=1
-    )
-    copied["y"] = copied.apply(
-        lambda x: (((x["y1"] + x["y2"]) / 2) % cell_size) / cell_size,
-        axis=1
-    )
-    copied["w"] = copied.apply(lambda x: (x["x2"] - x["x1"]) / img_size, axis=1)
-    copied["h"] = copied.apply(lambda x: (x["y2"] - x["y1"]) / img_size, axis=1)
-
-    copied["c"] = 1
-    copied["x_grid"] = copied.apply(
-        lambda x: int((x["x1"] + x["x2"]) / 2 / cell_size), axis=1
-    )
-    copied["y_grid"] = copied.apply(
-        lambda x: int((x["y1"] + x["y2"]) / 2 / cell_size), axis=1
-    )
-
-    gt = torch.zeros((30, n_cells, n_cells), dtype=torch.float64)
-    for tup in copied.itertuples():
-        _, _, _, _, _, obj, x, y, w, h, c, x_grid, y_grid = tup
-
-        gt[(0, 5), y_grid, x_grid] = x
-        gt[(1, 6), y_grid, x_grid] = y
-        gt[(2, 7), y_grid, x_grid] = w
-        gt[(3, 8), y_grid, x_grid] = h
-        gt[(4, 9), y_grid, x_grid] = c
-        gt[9 + obj, y_grid, x_grid] = 1
-    return gt
-
-
-class Transform(object):
-    def __call__(self, image):
-        h, w = image.size
-        transform = T.Compose(
-            [
-                T.ToTensor(),
-                T.CenterCrop(max(h, w)),
-                T.Resize(IMG_SIZE, antialias=True),
-                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ]
-        )
-        x = transform(image)
-        return x
-
-
+# class Transform(object):
+#     def __call__(self, image):
+#         h, w = image.size
+#         transform = T.Compose(
+#             [
+#                 T.ToTensor(),
+#                 T.CenterCrop(max(h, w)),
+#                 T.Resize(IMG_SIZE, antialias=True),
+#                 T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#             ]
+#         )
+#         x = transform(image)
+#         return x
 
 
 class VOC2012Dataset(Dataset):
-    def __init__(self, root, transform=None):
+    # def __init__(self, annot_dir, transform=None):
+    def __init__(self, annot_dir):
         super().__init__()
 
-        self.root = root
-        self.transform = transform
+        self.annots = list(Path(annot_dir).glob("*.xml"))
+        # self.transform = transform
+
+    def _randomly_flip_horizontally(self, image, bboxes, p=0.5):
+        w, _ = image.size
+        if random.random() > 1 - p:
+            image = TF.hflip(image)
+            bboxes["x1"], bboxes["x2"] = w - bboxes["x2"], w - bboxes["x1"]
+        return image, bboxes
 
     # "We introduce random scaling and translations of up to 20% of the original image size."
-    def _randomly_scale(self, image, bboxes, ratio=RATIO):
+    def _randomly_scale(self, image, bboxes):
         w, h = image.size
-        scale = random.uniform(1 - ratio, 1 + ratio)
+        scale = random.uniform(1 - TRANSFORM_RATIO, 1 + TRANSFORM_RATIO)
         image = TF.resize(image, size=(round(h * scale), round(w * scale)), antialias=True)
 
         bboxes["x1"] = bboxes["x1"].apply(lambda x: round(x * scale))
@@ -171,64 +116,108 @@ class VOC2012Dataset(Dataset):
         bboxes["y2"] = bboxes["y2"].apply(lambda x: round(x * scale))
         return image, bboxes
 
-    def _randomly_shift_and_resize(self, image, ori_w, ori_h, ratio=RATIO):
-        # We also randomly adjust the exposure and saturation of the image by up to a factor of 1:5 in the HSV color space."
-        dx = round(ori_w * random.uniform(-ratio, ratio))
-        dy = round(ori_h * random.uniform(-ratio, ratio))
-        w, h = image.size
-        padding=(
-            (IMG_SIZE - w) // 2 + dx,
-            (IMG_SIZE - h) // 2 + dy,
-            IMG_SIZE - w - (IMG_SIZE - w) // 2 - dx,
-            IMG_SIZE - h - (IMG_SIZE - h) // 2 - dy,
-        )
-        image = TF.pad(image, padding=padding, padding_mode="edge")
+    def _randomly_shift(self, image, bboxes, ori_w, ori_h):
+        dx = round(ori_w * random.uniform(-TRANSFORM_RATIO, TRANSFORM_RATIO))
+        dy = round(ori_h * random.uniform(-TRANSFORM_RATIO, TRANSFORM_RATIO))
+
+        image = TF.pad(image, padding=(dx, dy, -dx, -dy), padding_mode="constant")
+
+        bboxes["x1"] += dx
+        bboxes["y1"] += dy
+        bboxes["x2"] += dx
+        bboxes["y2"] += dy
+        return image, bboxes
+
+    def _randomly_adjust_b_and_s(self, image):
+        # We also randomly adjust the exposure and saturation of the image by up to a factor
+        # of 1.5 in the HSV color space."
+        image = TF.adjust_brightness(image, random.uniform(0.5, 1.5))
+        image = TF.adjust_saturation(image, random.uniform(0.5, 1.5))
         return image
 
+    def _crop_center(self, image, bboxes):
+        w, h = image.size
+        image = TF.center_crop(image, output_size=IMG_SIZE)
+        bboxes["x1"] += (IMG_SIZE - w) // 2
+        bboxes["y1"] += (IMG_SIZE - h) // 2
+        bboxes["x2"] += (IMG_SIZE - w) // 2
+        bboxes["y2"] += (IMG_SIZE - h) // 2
+        return image, bboxes
+
+    def _encode(self, bboxes):
+        bboxes["x"] = bboxes.apply(
+            lambda x: (((x["x1"] + x["x2"]) / 2) % CELL_SIZE) / CELL_SIZE,
+            axis=1
+        )
+        bboxes["y"] = bboxes.apply(
+            lambda x: (((x["y1"] + x["y2"]) / 2) % CELL_SIZE) / CELL_SIZE,
+            axis=1
+        )
+        bboxes["w"] = bboxes.apply(lambda x: (x["x2"] - x["x1"]) / IMG_SIZE, axis=1)
+        bboxes["h"] = bboxes.apply(lambda x: (x["y2"] - x["y1"]) / IMG_SIZE, axis=1)
+
+        bboxes["x_grid"] = bboxes.apply(
+            lambda x: int((x["x1"] + x["x2"]) / 2 / CELL_SIZE), axis=1
+        )
+        bboxes["y_grid"] = bboxes.apply(
+            lambda x: int((x["y1"] + x["y2"]) / 2 / CELL_SIZE), axis=1
+        )
+        bboxes
+
+        gt = torch.zeros((25, N_CELLS, N_CELLS), dtype=torch.float64)
+        for row in bboxes.itertuples():
+            gt[0, row.y_grid, row.x_grid] = row.x
+            gt[1, row.y_grid, row.x_grid] = row.y
+            gt[2, row.y_grid, row.x_grid] = row.w
+            gt[3, row.y_grid, row.x_grid] = row.h
+            gt[4, row.y_grid, row.x_grid] = 1
+            gt[5 + row.cls, row.y_grid, row.x_grid] = 1
+        return gt
+
     def __len__(self):
-        return len(list(Path(self.root).glob("*.xml")))
+        return len(self.annots)
 
     def __getitem__(self, idx):
-        xml_path = list(Path(self.root).glob("*.xml"))[idx]
-        xml_path = "/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/Annotations/2007_000032.xml"
-        for _ in range(10):
-            image, bboxes = parse_xml_file(xml_path)
-            ori_w, ori_h = image.size
-            image, bboxes = self._randomly_scale(image=image, bboxes=bboxes)
-            image = self._randomly_shift_and_resize(image, ori_w=ori_w, ori_h=ori_h)
-            image.show()
+        xml_path = self.annots[idx]
+        # xml_path = "/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/Annotations/2007_000032.xml"
+        # for _ in range(10):
+        image, bboxes = parse_xml_file(xml_path)
+        ori_w, ori_h = image.size
+
+        image, bboxes = self._randomly_flip_horizontally(image=image, bboxes=bboxes)
+        image, bboxes = self._randomly_scale(image=image, bboxes=bboxes)
+        image, bboxes = self._randomly_shift(
+            image=image, bboxes=bboxes, ori_w=ori_w, ori_h=ori_h
+        )
+        image = self._randomly_adjust_b_and_s(image)
+        image, bboxes = self._crop_center(image=image, bboxes=bboxes)
+
+        bboxes[["x1", "y1", "x2", "y2"]] = bboxes[["x1", "y1", "x2", "y2"]].clip(0, IMG_SIZE)
+        bboxes = bboxes[(bboxes["x1"] != bboxes["x2"]) & (bboxes["y1"] != bboxes["y2"])]
+        return image, bboxes
+
+        # gt = self._encode(bboxes)
+        # return image, gt
 
 
 if __name__ == "__main__":
-    transform = Transform()
-    ds = VOC2012Dataset(root="/Users/jongbeomkim/Downloads/VOCdevkit/VOC2012/Annotations", transform=transform)
-    dl = DataLoader(dataset=ds, batch_size=8, shuffle=True, drop_last=True, collate_fn=pad_to_bboxes)
-    for batch, (image, gt) in enumerate(dl, start=1):
-        image.shape, gt.shape
+    # transform = Transform()
+    ds = VOC2012Dataset(annot_dir="/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/Annotations")
+    # parse_xml_file(ds.annots[0])[1]
+    image, bboxes = ds[0]
 
-        grid = batched_image_to_grid(image=image, n_cols=4, normalize=True)
-        show_image(grid)
-
-        
-        darknet = Darknet()
-        yolo = YOLO(darknet=darknet, n_classes=20)
-        pred = yolo(image)
-
-
-def _randomly_adjust_brightness_and_saturation(image): # Exposure
-    h, s, v = cv2.split(np.array(image.convert("HSV")))
-    # v_scale = random.uniform(0.5, 1.5)
-    v_scale = 1.5
-    s_scale = random.uniform(0.5, 1.5)
-    v = np.clip((v * v_scale), 0, 255).astype("uint8")
-    # s = np.clip((s * s_scale), 0, 255).astype("uint8")
-    new_image = Image.fromarray(cv2.merge([h, s, v]), mode="HSV")
-    new_image.show()
-    return new_image
-
-
-
-
-image = Image.open("/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/JPEGImages/2007_000346.jpg").convert("RGB")
-new_image = T.ColorJitter((1.5, 1.5))(image)
-new_image.show()
+    draw = ImageDraw.Draw(image)
+    for row in bboxes.itertuples():
+        draw.rectangle(
+            xy=(row.x1, row.y1, row.x2, row.y2),
+            outline="rgb(255, 0, 0)",
+            fill=None,
+            width=2
+        )
+        draw.line(xy=(row.x1, row.y1, row.x2, row.y2), fill="rgb(255, 0, 0)", width=1)
+        draw.line(xy=(row.x1, row.y2, row.x2, row.y1), fill="rgb(255, 0, 0)", width=1)
+    for i in range(1, N_CELLS):
+        val = CELL_SIZE * i
+        draw.line(xy=(val, 0, val, IMG_SIZE), fill="rgb(0, 255, 0)", width=1)
+        draw.line(xy=(0, val, IMG_SIZE, val), fill="rgb(0, 255, 0)", width=1)
+    image.show()
