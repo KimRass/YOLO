30e060c1049350a94b0a6dd6efabc9956b5a2ece
KimRass <rmx1000@yonsei.ac.kr>
2023-08-16 00:14:27 +0900

    log.txt

--- infer.py
+++ infer.py
@@ -22,6 +22,19 @@ def decode(tensor):
     bboxes[:, (1, 6), ...] *= config.CELL_SIZE # y
     bboxes[:, (1, 6), ...] += torch.linspace(0, config.IMG_SIZE - config.CELL_SIZE, config.N_CELLS).unsqueeze(1)
 
+    x1 = (bboxes[:, (0, 5), ...] - bboxes[:, (2, 7), ...] / 2).round()
+    y1 = (bboxes[:, (1, 6), ...] - bboxes[:, (3, 8), ...] / 2).round()
+    x2 = (bboxes[:, (0, 5), ...] + bboxes[:, (2, 7), ...] / 2).round()
+    y2 = (bboxes[:, (1, 6), ...] + bboxes[:, (3, 8), ...] / 2).round()
+
+    bboxes[:, (0, 5), ...] = x1
+    bboxes[:, (1, 6), ...] = y1
+    bboxes[:, (2, 7), ...] = x2
+    bboxes[:, (3, 8), ...] = y2
+    bboxes[:, (0, 1, 2, 3, 5, 6, 7, 8), ...] = torch.clip(
+        bboxes[:, (0, 1, 2, 3, 5, 6, 7, 8), ...], min=0, max=config.IMG_SIZE
+    )
+
     bboxes1 = torch.cat([bboxes[:, : 5, ...], bboxes[:, 10:, ...]], dim=1)
     bboxes2 = torch.cat([bboxes[:, 5: 10, ...], bboxes[:, 10:, ...]], dim=1)
     bboxes1 = rearrange(bboxes1, pattern="b c h w -> b (h w) c")
@@ -36,6 +49,7 @@ def decode(tensor):
 class mAP
 
 model = YOLOv1()
+model.eval()
 ckpt_path = "/Users/jongbeomkim/Downloads/yolo_checkpoints/135.pth"
 ckpt = torch.load(ckpt_path, map_location="cpu")
 model.load_state_dict(ckpt["model"])
@@ -43,7 +57,7 @@ model.load_state_dict(ckpt["model"])
 ds = VOC2012Dataset(annot_dir="/Users/jongbeomkim/Documents/datasets/voc2012/VOCdevkit/VOC2012/Annotations")
 dl = DataLoader(
     ds,
-    batch_size=2,
+    batch_size=1,
     shuffle=False,
     # num_workers=config.N_WORKERS,
     num_workers=0,
@@ -56,16 +70,13 @@ for image, gt in dl:
 
     pred = model(image)
     pred_bboxes = decode(pred)
-
-    # pred_bboxes.shape, gt_bboxes.shape
     for pred_bbox, gt_bbox in zip(pred_bboxes, gt_bboxes):
-        pred_bbox = pred_bbox[pred_bbox[:, 4] >= 0.5]
-        pred_bbox = pred_bbox[torch.sort(pred_bbox[:, 4], dim=0)[1]]
+        pred_bbox = pred_bbox[pred_bbox[:, 4] * pred_bbox[:, 6] >= 0.9]
+        pred_bbox = pred_bbox[torch.sort(pred_bbox[:, 4], dim=0, descending=True)[1]]
+
+        iou = box_iou(pred_bbox[:, : 4], pred_bbox[:, : 4])
+        pred_bbox = pred_bbox[(torch.triu((iou >= 0.5), diagonal=1).sum(dim=0) == 0)]
+        pred_bbox
 
         gt_bbox = gt_bbox[gt_bbox[:, 4] >= 0.5]
-        # pred_bbox.shape, gt_bbox.shape
-        pred_bbox
-        
-        box_iou(pred_bbox[:, : 4], gt_bbox[:, : 4])
-        pred_bbox.shape
-        pred_bbox.shape
+        gt_bbox
